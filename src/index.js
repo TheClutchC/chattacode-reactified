11c7e0935da9fae4acb0f37cbe9c8fea896b19e3
Clay Carney <claycarney7@gmail.com>
2022-11-22 09:38:07 -0500

    add link to import for react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 import './App.scss';
 
 
