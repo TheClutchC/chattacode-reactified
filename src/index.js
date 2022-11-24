9fa452beb051dc5bca122ea776ce8a50cc561a5b
Clay Carney <claycarney7@gmail.com>
2022-11-24 15:14:26 -0500

    back to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter basename='/chattacode-reactified'>
+    <BrowserRouter basename='/chattacode-reactified'>
         <App />
-    </HashRouter>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
