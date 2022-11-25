7fb23615cf1fb6cc8b97c818d19764aae7eaac34
Clay Carney <claycarney7@gmail.com>
2022-11-25 15:05:48 -0500

    fix #3984573

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -10,21 +10,19 @@ import ProjectsPage from './pages/ProjectsPage';
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
-      <div className="App">
+    <HashRouter>
+      <nav>
         <NavBar />
-        <div>
-          <Routes>
-            <Route exact path='/about' element={<AboutPage />} />
-            <Route exact path='/events' element={<EventsPage />} />
-            <Route exact path='/connect' element={<ConnectPage />} />
-            <Route exact path='/blog' element={<BlogPage />} />
-            <Route exact path='/projects' element={<ProjectsPage />} />
-            <Route exact path='/' element={<HomePage />} />
-          </Routes>
-        </div>
-      </div>
-    </BrowserRouter>
+      </nav>
+      <Routes>
+        <Route exact path='/about' element={<AboutPage />} />
+        <Route exact path='/events' element={<EventsPage />} />
+        <Route exact path='/connect' element={<ConnectPage />} />
+        <Route exact path='/blog' element={<BlogPage />} />
+        <Route exact path='/projects' element={<ProjectsPage />} />
+        <Route exact path='/' element={<HomePage />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
