01f32082945812b77d2ba43f31b90f71fe3361d1
Clay Carney <claycarney7@gmail.com>
2022-11-24 20:55:45 -0500

    combine browserrouter and hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
@@ -10,19 +10,23 @@ import ProjectsPage from './pages/ProjectsPage';
 
 function App() {
   return (
-      <div className="App">
-        <NavBar />
-        <div>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/events' element={<EventsPage />} />
-            <Route path='/connect' element={<ConnectPage />} />
-            <Route path='/blog' element={<BlogPage />} />
-            <Route path='/projects' element={<ProjectsPage />} />
-          </Routes>
+    <BrowserRouter>
+      <HashRouter basename='/'>
+        <div className="App">
+          <NavBar />
+          <div>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/about' element={<AboutPage />} />
+              <Route path='/events' element={<EventsPage />} />
+              <Route path='/connect' element={<ConnectPage />} />
+              <Route path='/blog' element={<BlogPage />} />
+              <Route path='/projects' element={<ProjectsPage />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </HashRouter>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
 import './App.scss';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename='/chattacode-reactified'>
-        <App />
-    </BrowserRouter>
+    <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
