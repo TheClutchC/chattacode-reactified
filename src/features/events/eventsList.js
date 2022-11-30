8f9752b9b4c33a541492ffb058115252057f1789
Clay Carney <claycarney7@gmail.com>
2022-11-29 21:40:36 -0500

    add array of objects for in-person events

---
+++ src/features/events/eventsList.js
@@ -0,0 +1,90 @@
+
+
+const eventsList = () => [
+    {
+        id: 0,
+        day: "Monday",
+        date: "",
+        time: "6 p.m. - 8 p.m.",
+        name: "Coding in the Park",
+        location: "",
+        description: "",
+        stack: "",
+        isAttending: Boolean,
+        numberOfAttendees: Number
+    },
+    {
+        id: 1,
+        day: "Tueday",
+        date: "",
+        time: "6 p.m. - 8 p.m.",
+        name: "Learning Essentials ( HTML, CSS, Javascript )",
+        location: "",
+        description: "",
+        stack: "",
+        isAttending: Boolean,
+        numberOfAttendees: Number
+    },
+    {
+        id: 2,
+        day: "Wednesday",
+        date: "",
+        time: "6 p.m. - 8 p.m.",
+        name: "Learning Essentials ( HTML, CSS, Javascript )",
+        location: "",
+        description: "",
+        stack: "",
+        isAttending: Boolean,
+        numberOfAttendees: Number
+    },
+    {
+        id: 3,
+        day: "Thursday",
+        date: "",
+        time: "6 p.m. - 8 p.m.",
+        name: "Progression (Javascript, Bootstrap, React)",
+        location: "",
+        description: "",
+        stack: "",
+        isAttending: Boolean,
+        numberOfAttendees: Number
+    },
+    {
+        id: 4,
+        day: "Friday",
+        date: "",
+        time: "6 p.m. - 8 p.m.",
+        name: "Progression (Javascript, Bootstrap, React)",
+        location: "",
+        description: "",
+        stack: "",
+        isAttending: Boolean,
+        numberOfAttendees: Number
+    },
+    {
+        id: 5,
+        day: "Saturday",
+        date: "",
+        time: "3 p.m. - 8 p.m.",
+        name: "Partnered Project Building",
+        location: "",
+        description: "",
+        stack: "",
+        isAttending: Boolean,
+        numberOfAttendees: Number
+    },
+    {
+        id: 6,
+        day: "Sunday",
+        date: "",
+        time: "6 p.m. - 8 p.m.",
+        name: "Sundae Funday",
+        location: "",
+        description: "",
+        stack: "",
+        isAttending: Boolean,
+        numberOfAttendees: Number
+    }
+];
+
+export default eventsList;
