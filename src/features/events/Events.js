7d83ed9cf47cc26cf2fb201a8fc3cb813dc2a035
Clay Carney <claycarney7@gmail.com>
2023-01-07 00:38:42 -0500

    Continue work on event modal

--- src/features/events/Events.js
+++ src/features/events/Events.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { eventsData } from "../../app/data/eventsData";
-import { Row, Col, Card, CardTitle, CardSubtitle, CardText } from "reactstrap";
+import { 
+  Row, 
+  Col, 
+  Card, 
+  CardTitle, 
+  CardSubtitle, 
+  CardText, 
+  Button, 
+  Modal, 
+  ModalHeader, 
+  ModalBody, 
+  ModalFooter 
+} from "reactstrap";
 
 //-------Below maps through array of objects(eventsData.js) and returns requested info using card layout in Reactstrap---------
-  const Events = () => {
+  const Events = (args) => {
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+
     return (
       <Row>
         <Col>
@@ -14,7 +30,7 @@ import { Row, Col, Card, CardTitle, CardSubtitle, CardText } from "reactstrap";
               color="dark"
               outline
             >
-              <CardTitle className="h4 pb-2" key={item.day}>
+              <CardTitle className="h4 pb-2 pt-1" key={item.day}>
                 {item.day}
               </CardTitle>
               <CardSubtitle className="h6" key={item.eventName}>
@@ -23,6 +39,41 @@ import { Row, Col, Card, CardTitle, CardSubtitle, CardText } from "reactstrap";
               <CardText className="h6" key={item.time}>
                 {item.time}
               </CardText>
+
+              {/* Below is code for a modal
+               that pops up when clicked for
+                each day of the week */}
+
+              <Button key={item.day} onClick={toggle}>
+                Click here for more info about {item.day}'s meeting
+              </Button>
+              <Modal
+                isOpen={modal} 
+                toggle={toggle} 
+                {...args}
+              >
+                <ModalHeader 
+                  toggle={toggle} 
+                  key={item.day}
+                >
+                  {item.day}
+                </ModalHeader>
+                <ModalBody key={item.description}>
+                  {item.description}
+                </ModalBody>
+                <ModalFooter>
+
+                </ModalFooter>
+              </Modal>
+              {/* This is the end of
+               the code for the modal.
+               Code seems to break once 
+               modal is toggled, only 
+               displays info for Sunday
+               on every button and loses 
+               the ability to scroll up 
+               and down the page. */}
+
             </Card>
           ))}
         </Col>
